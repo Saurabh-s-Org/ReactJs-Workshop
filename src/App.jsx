dcc2bd45ecfc9c2ff19e557e02489f5cbe4bca1a
Saurabh Dixit <86460997+Saurabhdixit93@users.noreply.github.com>
2024-10-02 16:08:01 +0530

    Update App.jsx
    added signup component

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup";
+
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div>
       <Login />
+{/*       <Signup /> */}
     </div>
   );
 }
